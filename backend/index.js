14e005334e7806f203ab51b440eedb531f03d12f
mar-ilyn <marilyndiazabc160@gmail.com>
2026-02-01 22:59:59 -0300

    add backend express server

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+
+let tasks = [
+  { id: 1, title: "Hacer la app", done: false }
+];
+
+app.get("/api/tasks", (req, res) => {
+  res.json(tasks);
+});
+
+app.post("/api/tasks", (req, res) => {
+  const newTask = {
+    id: Date.now(),
+    title: req.body.title,
+    completed: false
+  };
+  tasks.push(newTask);
+  res.status(201).json(newTask);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
